5865048f677a107e124096d46e6a3c4ad8da1a56
Riya <riya.v.sinha@gmail.com>
2018-10-11 19:12:35 -0700

    import only necessary firebase pacakages to stop warning on page load

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
   apiKey: "AIzaSyDtTf6JazrP_0yGD2cOarEBeGDw2KV2Tlo",
