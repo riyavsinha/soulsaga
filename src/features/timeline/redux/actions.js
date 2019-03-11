e818d8ee22c6046067bc27376e34e6dc3b0d4a74
Riya <riya.v.sinha@gmail.com>
2019-03-10 17:12:44 -0700

    delete populateEvents action. Is not in use.

--- src/features/timeline/redux/actions.js
+++ src/features/timeline/redux/actions.js
@@ -2,7 +2,6 @@ export { addEvent, dismissAddEventError } from './addEvent';
 export { deleteEvent, dismissDeleteEventError } from './deleteEvent';
 export { toggleAddEventForm } from './toggleAddEventForm';
 export { setEditingEvent } from './setEditingEvent';
-export { populateEvents } from './populateEvents';
 export { setViewingEvent } from './setViewingEvent';
 export { setDeletingEvent } from './setDeletingEvent';
 export { deleteUserEvents, dismissDeleteUserEventsError } from './deleteUserEvents';
