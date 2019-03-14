0ff94fa29e11f12db3615a6bfae6d4155f776efb
Riya <riya.v.sinha@gmail.com>
2019-03-14 15:12:54 -0700

    working config, tested locally

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,6 +1,6 @@
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=416dfbbf1fa2c5be6b0693249ae85637598c6423\
+    --form revision=0ca6bf9d462fd646bfd8ef9e49a6bf2dec4ca23a\
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/riyavsinha/soulsaga/tree/testing
